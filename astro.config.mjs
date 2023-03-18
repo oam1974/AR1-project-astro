f3cd6e47677ecf3f23421704bfb062f674f03e73
oam1974 <oam1974@gmail.com>
2023-03-18 00:23:51 -0300

    v03_test inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://oam1974.github.io',
-    base: '/AR1-project-astro',
-    // server: { port: 3000, host: '192.168.0.9'}
+    // site: 'https://oam1974.github.io',
+    // base: '/AR1-project-astro',
+    server: { port: 3000, host: '192.168.0.9'}
       
 });
